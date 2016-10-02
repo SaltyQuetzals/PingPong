d144a35d5e8e4480e67d6203b470173be65dc56e
jaflo <imjaflo@gmail.com>
2016-10-02 05:31:37 +0200

    Added content to client

---
+++ client/www/client.js
@@ -0,0 +1,205 @@
+$("#tags input").tagsInput({
+	"defaultText": "Add an interest...",
+	"onChange": function() {
+		// $.post(root+"/register/verify", {
+		// 	cc: $("#prefix").val(),
+		// 	phone: $(".telephone input").val(),
+		// 	SMScode: $(".verify input").val()
+		// }).done(function(res) {
+		// 	if (res.status == "success") {
+		// 		userFlow = 1;
+		// 		localStorage.setItem("userFlow", 1);
+		// 		backAction(false);
+		// 		viewUpdate();
+		// 	} else {
+		// 		alert("An error occurred");
+		// 	}
+		// });
+	},
+	//"delimiter": [",", ";", " "],
+	"minChars" : 0,
+	"maxChars" : 0
+});
+
+$(document).ajaxError(function() {
+	alert("The server seems to be offline. Are you connected to the internet?");
+});
+
+var token = localStorage.getItem("token"),
+	 userFlow = localStorage.getItem("userFlow") || 1,
+	 backable = false,
+	 root = "/";
+
+$("#back").click(function() {
+	if (backable) {
+		backable();
+		backAction(false);
+	}
+});
+
+function backAction(f) {
+	backable = f;
+	$("#back").toggle(backable !== false);
+}
+
+backAction(false);
+
+$(".view").not("#loader").hide();
+
+function replaceView(hide, show) {
+	$(".view").not(hide+","+show).hide();
+	$(show).show().removeClass("hidden");
+	$(hide).addClass("hidden");
+	setTimeout(function() {
+		$(hide).addClass("gone").hide();
+	}, 350);
+}
+
+function viewUpdate() {
+	if (userFlow == 0) { // welcome
+		$(codes.countries).each(function() {
+			$("#prefix").append($("<option>").attr("value", this.code).text(this.name+" ("+this.code+")"));
+		});
+		$("#prefix option[value='+1']").attr("selected", "true");
+		replaceView("#loader", "#intro");
+		$(".telephone").submit(function() {
+			if (!$(".telephone input").val()) {
+				alert("Please enter a telephone number.");
+				return false;
+			}
+			$.post(root+"/register", {
+				cc: $("#prefix").val(),
+				phone: $(".telephone input").val()
+			}).done(function(res) {
+				if (res.status == "success") {
+					$(".slidey").addClass("second");
+					backAction(function() {
+						$(".slidey").removeClass("second");
+					});
+					$(".verify").submit(function() {
+						if (!$(".verify input").val()) {
+							alert("Please enter your verification code.");
+							return false;
+						}
+						$.post(root+"/register/verify", {
+							cc: $("#prefix").val(),
+							phone: $(".telephone input").val(),
+							SMScode: $(".verify input").val()
+						}).done(function(res) {
+							if (res.status == "success") {
+								userFlow = 1;
+								localStorage.setItem("userFlow", 1);
+								token = res.data.token;
+								localStorage.setItem("token", token);
+								backAction(false);
+								viewUpdate();
+							} else {
+								alert("An error occurred");
+							}
+						});
+						return false;
+					});
+				} else {
+					alert("An error occurred");
+				}
+			});
+			return false;
+		});
+	} else if (userFlow == 1) { //
+		replaceView("#intro", "#main");
+		$("#main button").click(function() {
+			if (true) {
+				$("#map").addClass("collapsed");
+				$("#recent").addClass("pushedout");
+				setTimeout(function() {
+					$("#recent").hide();
+				}, 300);
+				$(".tagsinput").addClass("selectable");
+				backAction(function() {
+					$("#map").removeClass("collapsed");
+					$("#recent").show().removeClass("pushedout");
+					$(".tagsinput").removeClass("selectable");
+					$(".tagsinput span").unbind("click");
+				});
+				$(".tagsinput span").click(function() {
+					$(this).toggleClass("active");
+				});
+				var ping = {};
+				navigator.geolocation.getCurrentPosition(function(position) {
+					ping.lat = position.coords.latitude;
+					ping.long = position.coords.longitude;
+				}, function(error) {
+					alert(error.message);
+				}, {
+					maximumAge: 3000,
+					timeout: 5000,
+					enableHighAccuracy: true
+				});
+			}
+		});
+		$("#recent a").click(function() {
+			return false;
+		});
+		/*var push = PushNotification.init({
+			"android": {
+				"senderID": "XXXXXXXX"
+			},
+			"ios": {
+				"sound": true,
+				"vibration": true,
+				"badge": true
+			},
+			"windows": {}
+		});
+
+		push.on('registration', function(data) {
+			console.log('registration event: ' + data.registrationId);
+
+			var oldRegId = localStorage.getItem('registrationId');
+			if (oldRegId !== data.registrationId) {
+				// Save new registration ID
+				localStorage.setItem('registrationId', data.registrationId);
+				// Post registrationId to your app server as the value has changed
+			}
+
+			var parentElement = document.getElementById('registration');
+			var listeningElement = parentElement.querySelector('.waiting');
+			var receivedElement = parentElement.querySelector('.received');
+
+			listeningElement.setAttribute('style', 'display:none;');
+			receivedElement.setAttribute('style', 'display:block;');
+		}).on('error', function(e) {
+			console.log("push error = " + e.message);
+		}).on('notification', function(data) {
+			console.log('notification event');
+			navigator.notification.alert(
+				data.message,		 // message
+				null,				 // callback
+				data.title,		   // title
+				'Ok'				  // buttonName
+			);
+		});*/
+	}
+}
+
+function replaceMap(c) {
+	$("#map .image").css("background-image", "url(https://maps.googleapis.com/maps/api/staticmap?size=640x400&zoom=14&center="+c+"&scale=2&style=feature:water|element:geometry|color:0x165c64|saturation:34|lightness:-69|visibility:on&style=feature:landscape|element:geometry|color:0xb7caaa|saturation:-14|lightness:-18|visibility:on&style=feature:landscape.man_made|element:all|color:0xcbdac1|saturation:-6|lightness:-9|visibility:on&style=feature:road|element:geometry|color:0x8d9b83|saturation:-89|lightness:-12|visibility:on&style=feature:road.highway|element:geometry|color:0xd4dad0|saturation:-88|lightness:54|visibility:simplified&style=feature:road.arterial|element:geometry|color:0xbdc5b6|saturation:-89|lightness:-3|visibility:simplified&style=feature:road.local|element:geometry|color:0xbdc5b6|saturation:-89|lightness:-26|visibility:on&style=feature:poi|element:geometry|color:0xc17118|saturation:61|lightness:-45|visibility:on&style=feature:poi.park|element:all|color:0x8ba975|saturation:-46|lightness:-28|visibility:on&style=feature:transit|element:geometry|color:0xa43218|saturation:74|lightness:-51|visibility:simplified&style=feature:administrative.province|element:all|color:0xffffff|saturation:0|lightness:100|visibility:simplified&style=feature:administrative.neighborhood|element:all|color:0xffffff|saturation:0|lightness:100|visibility:off&style=feature:administrative.locality|element:labels|color:0xffffff|saturation:0|lightness:100|visibility:off&style=feature:administrative.land_parcel|element:all|color:0xffffff|saturation:0|lightness:100|visibility:off&style=feature:administrative|element:all|color:0x3a3935|saturation:5|lightness:-57|visibility:off&style=feature:administrative|element:all|color:0x3a3935|saturation:5|lightness:-57|visibility:off&style=feature:poi.medical|element:geometry|color:0xcba923|saturation:50|lightness:-46|visibility:on)");
+}
+
+function updateLoc() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		replaceMap(position.coords.latitude+","+position.coords.longitude);
+	}, function(error) {
+		alert(error.message);
+	}, {
+		maximumAge: 3000,
+		timeout: 5000,
+		enableHighAccuracy: true
+	});
+}
+
+updateLoc();
+
+replaceMap("40.714728,-73.998672");
+
+viewUpdate();
